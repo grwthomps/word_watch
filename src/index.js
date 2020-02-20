fdb42794234c7113b4713a1340372c32305925b5
Graham Thompson <grwthomps@gmail.com>
2020-02-20 10:45:54 -0700

    Implement post request for user input to word watch api

--- src/index.js
+++ src/index.js
@@ -10,4 +10,26 @@ $(document).ready(() => {
     var topWordCount = Object.values(myJson.word)[0]
     document.getElementById("top-word").innerHTML = topWord + " - " + topWordCount;
   });
+
+  document.getElementById ("submit-button").addEventListener ("click", submitWord, false);
 })
+
+function submitWord() {
+  var user_input = document.getElementById("text-box").value.replace(/\s+/g, '').replace(/[^A-Za-z]/g, '')
+
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+    method: 'post',
+    body: JSON.stringify({
+      word: {
+        value: user_input
+      }
+    }),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  }).then((response) => {
+    return response.json();
+  }).then((jsonResponse) => {
+    console.log(jsonResponse)
+  });
+}
